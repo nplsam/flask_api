f12b926c9feb970eef64c976f36ed0a75a756025
nplsam <psammerrick@gmail.com>
2023-09-20 15:07:48 +0100

    added elephant sql url to .env

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+# this is the sorting order that the data in the API is read
+app.json_provider_class.sort_keys = False
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
+
+db = SQLAlchemy(app)
